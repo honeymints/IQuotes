a57475bda2423108d7b349451374bbdba95306a0
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-10-16 19:12:10 +0600

    database testing has added

---
+++ Database.Test/DatabaseFixture.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Database.Test;
+
+using IQuotes.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseFixture : IDisposable
+{
+    public ApplicationDbContext Context { get; }
+    private const string ConnectionUrl = @"Host=localhost;Port=5432;Database=quotes;Username=postgres;Password=gudron; Include Error Detail=true";
+
+
+    public DatabaseFixture()
+    {
+        var connectionString = ConnectionUrl;
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(connectionString).Options;
+        Context = new ApplicationDbContext(options);
+
+    }
+    public void Dispose()
+    {
+        Context.Database.EnsureDeleted();
+        Context.Dispose();
+    }
+}
