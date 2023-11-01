efd8ceef8b606315d9b4a84f3b2f2b19c1c58a30
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-11-01 14:18:38 +0600

    ui testing added

---
+++ UITesting/RegistrationUITest.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using Xunit;
+
+
+namespace UITesting;
+
+public class RegistrationUITest : UITest
+{
+
+    [Fact]
+    public void UserRegistration()
+    {
+        Driver.Navigate().GoToUrl("http://localhost:5210/Account/SignUp");
+
+        IWebElement usernameField = Driver.FindElement(By.Id("username"));
+        IWebElement emailField = Driver.FindElement(By.Id("email"));
+        IWebElement passwordField = Driver.FindElement(By.Id("psw"));
+        IWebElement confirmPasswordField = Driver.FindElement(By.Id("psw-repeat"));
+        IWebElement registerButton = Driver.FindElement(By.ClassName("registerbtn"));
+
+        // Fill in registration form
+        usernameField.SendKeys("testuser");
+        emailField.SendKeys("testuser@example.com");
+        passwordField.SendKeys("TestPassword1!");
+        confirmPasswordField.SendKeys("TestPassword1!");
+        
+
+        string initialUrl = Driver.Url; //initial current url
+
+        registerButton.Click();
+
+        // get the URL after clicking the button
+        string finalUrl = Driver.Url;
+
+        // assert that the URL changed to the home page
+        Assert.NotEqual(initialUrl, finalUrl);
+        Assert.Contains("http://localhost:5210/Home", finalUrl);
+
+    }
+}
+
---
+++ UITesting/UITest.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UITesting;
+
+
+
+public class UITest
+{
+    protected IWebDriver Driver;
+
+    public UITest()
+    {
+        Driver = new ChromeDriver(); // initialize Chrome WebDriver
+    }
+
+    public void Cleanup()
+    {
+        Driver.Quit(); // cleanup after the test
+    }
+}
